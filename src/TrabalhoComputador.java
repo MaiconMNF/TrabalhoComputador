ac4a24e6c39ec6c20b49cc9ef193fb51a7ccc11e
JussanJ <jussanjaeger@hotmail.com>
2016-11-14 10:57:06 -0200

    Desenho

--- src/TrabalhoComputador.java
+++ src/TrabalhoComputador.java
@@ -2,7 +2,31 @@
 public class TrabalhoComputador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" _______________                                 ____MEMÓRIA____");
+		System.out.println("| _____________ |                             10|_______________|");
+		System.out.println("||      UC     ||                             0F|_______________|");
+		System.out.println("||_____________||                             0E|_______________|");
+		System.out.println("| _|___     |   |                             0D|_______________|");
+		System.out.println("||_ULA_|    |   |                             0C|_______________|");
+		System.out.println("| _|________|__ |                             0B|_______________|");
+		System.out.println("||      A      ||                             0A|_______________|");
+		System.out.println("||_____________||                             09|_______________|");
+		System.out.println("||      B      ||                             08|_______________|");
+		System.out.println("||_____________||                             07|_______________|       ______VIDEO______");
+		System.out.println("||      PC     ||                             06|_______________|      | _______________ |");
+		System.out.println("||_____________||                             05|_______________|      ||               ||");
+		System.out.println("||      IR     ||                             04|_______________|      ||               ||");
+		System.out.println("||_____________||                             03|_______________|      ||               ||");
+		System.out.println("||    FLAGS    ||                             02|_______________|      ||               ||");
+		System.out.println("||_____________||                             01|_______________|      ||_______________||");
+		System.out.println("|_______________|                             00|_______________|      |_________________|");
+		System.out.println("  |   |   |   |                                     |   |   |               |   |   |");
+		System.out.println("  |   |   |   |                                     |   |   |               |   |   |");
+		System.out.println("  |   |   |   |_____________________________________|___|___|_______________|___|___|______Dados:");
+		System.out.println("  |   |   |_____________________________________________|___|___________________|___|______Endereço:");
+		System.out.println("  |   |_____________________________________________________|_______________________|______Read=0");
+		System.out.println("  |________________________________________________________________________________________Memória=0");
+		
 
 	}
 
